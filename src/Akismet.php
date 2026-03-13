d302c3f4c0a0d57a9aa79ddde9c351c146048730
Derek Springer <derek@automattic.com>
2026-03-13 13:55:02 -0700

    Add integration tests for getStats()
    Also fix getStats() to pass 'key' parameter (endpoint requires it, not 'api_key').

--- src/Akismet.php
+++ src/Akismet.php
@@ -167,6 +167,7 @@ final class Akismet implements AkismetInterface {
 		$response = $this->httpClient->post(
 			'/1.2/get-key-stats',
 			[
+				'key'  => $this->config->apiKey,
 				'from' => $interval->value,
 			]
 		);
